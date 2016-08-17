0126f34383e7ac55d76b0f3856c337abf639e93b
ahmedReda1 <se_ahmedreda@yahoo.com>
2016-08-17 16:02:03 +0200

    new permissions to fix run command && listing page

--- image-me/app/pages/gallery/datapipe.ts
+++ image-me/app/pages/gallery/datapipe.ts
@@ -4,11 +4,15 @@ import {Component ,Pipe ,PipeTransform } from '@angular/core';
 export class DataPipe  implements PipeTransform {
 
   transform(value) : any{
-  	
-   	let keys = [];
-	for (let key in value) {
-		console.log(key.image)
+	   	let keys = [];
+		for( var i=0;i<value.length;i++){
+			var imageData = value[i].image; 
+ 		    var dataUriPrefix = "data:image/	png;base64,";
+		    var img = document.createElement("IMG");
+		    img.setAttribute('src', dataUriPrefix + imageData);
+		    img.setAttribute('width', '200');
+		 	value[i].image = img;
+		}
+	    return value;
 	}
-
-    return keys;
 }
