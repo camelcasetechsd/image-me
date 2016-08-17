5bc3b73e5c3c1f8a21ad3a1b6d164845fa22ea88
Mohamed Labib <lebaz20@gmail.com>
2016-08-17 12:01:41 +0200

    Add form post request using normal XHR as angular does not provide full support yet

--- image-me-mobile/app/pages/upload/upload.ts
+++ image-me-mobile/app/pages/upload/upload.ts
@@ -1,19 +1,17 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
-import {FORM_DIRECTIVES, Validators, FormBuilder, FormGroup, FormControl} from '@angular/forms';
-import {HTTP_PROVIDERS, Http} from '@angular/http';
+import {FORM_DIRECTIVES, Validators, FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   templateUrl: 'build/pages/upload/upload.html',
   directives: [FORM_DIRECTIVES],
-  providers: [HTTP_PROVIDERS]
 })
 export class UploadPage {
     uploadForm: FormGroup;
     title: FormControl;
     image: FormControl;
  
-    constructor(private http: Http, private navController: NavController, private fb: FormBuilder) {
+    constructor(private navController: NavController) {
       this.uploadForm = new FormGroup({ 
         title: new FormControl('', Validators.required),
         image: new FormControl('', Validators.required)
@@ -22,16 +20,15 @@ export class UploadPage {
   uploadImage(event) {
     let titleValue = event.target[0].value;
     let imageFile = event.target[2].files[0];
-    let submittedData = {title: titleValue, image:imageFile};
     let path = 'http://localhost:8000/images';
-
-     return new Promise((resolve, reject) => {
-           var formData: any = new FormData();
+    let method = 'POST';
+    return new Promise((resolve, reject) => {
+           var formData: any = new FormData();
            var xhr = new XMLHttpRequest();
            
            formData.append('userId',123);
-           formData.append('title',titleValue);
-           formData.append('image',imageFile);  
+           formData.append('title',titleValue);
+           formData.append('image',imageFile)   
    
            xhr.onreadystatechange = function () {
                if (xhr.readyState == 4) {
@@ -42,11 +39,9 @@ export class UploadPage {
                    }
                }
            }
-           xhr.open("POST", path, true);
+           xhr.open(method, path, true);
            xhr.send(formData);
-       });
-
-    // this.http.post(path, JSON.stringify(submittedData));
-    // event.preventDefault();
+       })
   }
+
 }
