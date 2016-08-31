89dfacaf31e6d86d1ad64401bfe9656d244338d5
ahmedReda1 <se_ahmedreda@yahoo.com>
2016-08-31 13:49:03 +0200

    new grid view

--- mobile/app/pages/gallery/datapipe.ts
+++ mobile/app/pages/gallery/datapipe.ts
@@ -1,18 +1,22 @@
-import {Component ,Pipe ,PipeTransform } from '@angular/core';
+import {Component, Pipe, PipeTransform} from '@angular/core';
 
-@Pipe({name: 'keyValues'})
+@Pipe({name: 'threePerRow'})
 export class DataPipe  implements PipeTransform {
 
   transform(value) : any{
-	   	let keys = [];
-		for( var i=0;i<value.length;i++){
-			var imageData = value[i].image; 
- 		    var dataUriPrefix = "data:image/	png;base64,";
-		    var img = document.createElement("IMG");
-		    img.setAttribute('src', dataUriPrefix + imageData);
-		    img.setAttribute('width', '200');
-		 	value[i].image = img;
-		}
-	    return value;
+  	let container: any = [];
+	   	for(let i = 0 ; i < Math.ceil(value.length/3) ; i++ ){
+	   		let sub: any = [];
+	   		for(let j = 0 ; j < 3 ; j++ ){
+	   			if(typeof (value[(i*3)+j]) === "undefined"){
+	   				break;
+	   			}
+   				sub[j] = value[(i*3)+j]	   			
+
+	   		}	
+	   		container.push(sub);
+	   	}
+	   	console.log(container)
+	    return container;
 	}
 }
